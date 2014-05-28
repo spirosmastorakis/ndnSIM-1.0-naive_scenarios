d0fd380eb429b8b18580b82abcde3a8da1051d88
Spyridon Mastorakis <spiros.mastorakis@gmail.com>
2014-05-28 13:39:30 +0300

    Custom consumer app

---
+++ Internet2:OS3E/custom-consumer.h
@@ -0,0 +1,38 @@
+//custom-consumer.h
+
+#ifndef CUSTOM_CONS_H_
+#define CUSTOM_CONS_H_
+
+#include "ns3/ndn-app.h"
+#include "ns3/ndn-pit.h"
+
+namespace ns3 {
+
+class CustomConsumer : public ndn::App
+{
+public:   
+  static TypeId
+  GetTypeId ();
+  
+  virtual void
+  StartApplication ();
+ 
+  virtual void
+  StopApplication ();
+ 
+  virtual void
+  OnData (Ptr<const ndn::Data> contentObject);
+
+private:
+  ndn::Name prefix;
+  double frequency;
+
+  void 
+  SendInterest (/*Ptr<ndn::Pit> pit, Ptr<ndn::fib::Entry> fibEntry*/);
+
+};
+
+} // namespace ns3
+
+#endif 
+
