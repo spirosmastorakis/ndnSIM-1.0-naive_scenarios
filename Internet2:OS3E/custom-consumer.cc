05a114d3afdc8dfcabc4f5b8d67c89c3317492df
Spyridon Mastorakis <spiros.mastorakis@gmail.com>
2014-05-28 21:11:57 +0300

    Custom consumer app

--- Internet2:OS3E/custom-consumer.cc
+++ Internet2:OS3E/custom-consumer.cc
@@ -11,9 +11,6 @@
 
 #include "ns3/ndn-interest.h"
 #include "ns3/ndn-data.h"
-
-#include "ns3/ndn-pit.h"
-#include "ns3/ndn-fib.h"
 #include "ns3/random-variable.h"
 
 NS_LOG_COMPONENT_DEFINE ("CustomConsumer");
@@ -30,7 +27,7 @@ CustomConsumer::GetTypeId ()
     .SetParent<ndn::App> ()
     .AddConstructor<CustomConsumer> ()
     .AddAttribute ("Prefix", "data prefix", StringValue ("/"),MakeNameAccessor (&CustomConsumer::prefix),ndn::MakeNameChecker ())
-    .AddAttribute ("Frequency", "Frequency of interest packets", StringValue ("1.0"),MakeDoubleAccessor (&CustomConsumer::frequency), MakeDoubleChecker<double> ());
+    .AddAttribute ("Frequency", "Frequency of interest packets", StringValue ("1.0"),MakeDoubleAccessor (&CustomConsumer::frequency), MakeDoubleChecker<double> ()); 
   return tid;
 }
 
@@ -40,19 +37,9 @@ CustomConsumer::StartApplication ()
 {
   //ndn::App
   ndn::App::StartApplication (); 
-  
-  //FIB and PIT object
-  //Ptr<ndn::Fib> fib = GetNode ()->GetObject<ndn::Fib> ();
-  Ptr<ndn::Pit> pit = GetNode ()->GetObject<ndn::Pit> ();
-
-  //Set maximum pit entry lifetime
-  pit->SetMaxPitEntryLifetime (Seconds(1.0));
-  
-  //Add entry to FIB
-  //Ptr<ndn::fib::Entry> fibEntry = fib->Add (prefix, m_face, 0);
-  CustomConsumer::SendInterest(/*pit, fibEntry*/);  
-
-  //Simulator::Schedule (Seconds (1.0), CustomConsumer::SendInterest(pit, fibEntry), this);
+  //TODO: Implement interest sending policy
+  Simulator::Schedule (Seconds (1.0), &CustomConsumer::SendInterest, this);
+  Simulator::Schedule (Seconds (8.0), &CustomConsumer::SendInterest, this);
 }
 //Application is stopped
 void
@@ -63,50 +50,36 @@ CustomConsumer::StopApplication ()
 }
 
 void
-CustomConsumer::SendInterest (/*Ptr<ndn::Pit> pit, Ptr<ndn::fib::Entry> fibEntry*/)
+CustomConsumer::SendInterest ()
 {  
   //Create and configure ndn::Interest
   Ptr<ndn::Interest> interest = Create<ndn::Interest> ();
+  srand(time(NULL));
+  int num = rand();
+  ndn::Name prefix_without_seq = prefix;
+  //Random sequence number
+  //TODO: Implement retransmission policy
+  prefix.appendSeqNum (num);  
   UniformVariable rand (0,std::numeric_limits<uint32_t>::max ());
   interest->SetNonce            (rand.GetValue ());
   interest->SetName             (prefix);
   interest->SetInterestLifetime (Seconds (1.0));
   
   NS_LOG_DEBUG ("Sending Interest packet for " << prefix);
- /*
-  //PIT Lookup
-  Ptr<ndn::pit::Entry> pitEntry = pit->Lookup(*interest);
 
-  if (pitEntry == 0) {
-        //Add PIT entry
-        ndn::pit::Entry (*pit, interest, fibEntry);
-        ndn::pit::IncomingFace (m_face);
-  }
-  else {
-        //Update pitEntry life time
-        (*pitEntry).UpdateLifetime (Seconds (1.0));
-  	//Check if the current incoming face has been added to the list of incoming faces
-        const std::set<ndn::pit::IncomingFace>& faces = (*pitEntry).GetIncoming ();
-        if (faces.find (m_face) == faces.end ())
-		//If it has not been added then add it!
-		(*pitEntry).AddIncoming (m_face);
-  }
-*/
   //Call trace
   m_transmittedInterests (interest, this, m_face);
 
   m_face->ReceiveInterest (interest); 
 }
 
+
 //Data arrives
 void
 CustomConsumer::OnData (Ptr<const ndn::Data> contentObject)
 {
   NS_LOG_DEBUG ("Receiving Data packet for " << contentObject->GetName ());
-
-  //TODO: Content store  
-
-  //std::cout << "DATA received for name " << contentObject->GetName () << std::endl;
+ 
 }
 
-} //ns3
+
